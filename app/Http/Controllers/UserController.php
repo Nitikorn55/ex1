d34a4dcd8b07ea691e621b36bb2efaec7ccd1cc9
Nitikorn Kanta <kooncakeza55@gmail.com>
2020-08-15 15:04:31 +0700

    เข้าใจ Route Controller และ View นิดหน่อย

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(){
+        return "หน้า user";
+    }
+
+    public function create(){
+        return "สร้าง user";
+    }
+
+    public function showName ($name){
+        return "สวัสดี $name";
+    }
+    public function showName ($name,$age){
+        return "สวัสดี $name อายุ $age";
+    }
+}
